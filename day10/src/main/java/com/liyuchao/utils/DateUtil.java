99b5247fbe068fec201b167d8566b72b0b88842a
liyuchao905048824 <905048824@qq.com>
2019-09-11 08:22:40 +0800

    day10test

---
+++ day10/src/main/java/com/liyuchao/utils/DateUtil.java
@@ -0,0 +1,29 @@
+package com.liyuchao.utils;
+
+import java.sql.Date;
+
+public class DateUtil {
+	public static int getAge(Date src){
+		return 0;
+		
+	}
+	
+	public static Date getDateByMonthInit(Date src) {
+		
+		return null;
+	}
+	
+	public static Date getDayeByMonthLast (Date src) {
+		return null;
+	}
+	public static int getDaysByFuture (Date future) {
+		return 0;
+		
+	}
+	public static int getDaysByDeparted (Date departed) {
+		return 0;
+		
+	}
+	
+
+}
